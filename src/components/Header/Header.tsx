bd777684087bb54850346818d61420f9b3106451
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-11-05 01:57:57 +0200

    update: Header markup & styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,11 +11,11 @@ const Header: React.FC = async () => {
   const settings = await client.getSingle("settings");
 
   return (
-    <header>
+    <header className={css.header}>
       <div className={css.container}>
         <Link href={"/"}>{settings.data.site_title}</Link>
         <nav className={css.navigation}>
-          <ul>
+          <ul className={css.navigationList}>
             {settings.data.navigation.map(({ label, link }) => (
               <li key={label}>
                 <PrismicNextLink className={css.link} field={link}>
@@ -25,6 +25,9 @@ const Header: React.FC = async () => {
             ))}
           </ul>
         </nav>
+        <button type="button" className={css.menuBtn}>
+          <MenuIcon />
+        </button>
         <button className={css.bookingBtn} type="button">
           Booking
         </button>
@@ -32,5 +35,25 @@ const Header: React.FC = async () => {
     </header>
   );
 };
+
+function MenuIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+    >
+      <g clipPath="url(#a)">
+        <path d="M26.667 24.167H3.333a.833.833 0 110-1.667h23.334a.833.833 0 010 1.667zm0-8.334H3.333a.834.834 0 010-1.666h23.334a.834.834 0 010 1.666zm0-8.333H3.333a.833.833 0 010-1.667h23.334a.833.833 0 010 1.667z"></path>
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path d="M0 0h30v30H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
 
 export default Header;
