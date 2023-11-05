83dbbc72a81a62a06d7a425df180eb48196f92bd
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-11-05 18:10:32 +0200

    update: section

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -5,16 +5,24 @@ import clsx from "clsx";
 interface IProps {
   as?: React.ElementType;
   className?: string;
+  style?: "dark" | "light";
   children: React.ReactNode;
 }
 
 const Section: React.FC<IProps> = ({
   as: Comp = "section",
   className,
+  style = "dark",
   children,
 }) => {
   return (
-    <Comp className={clsx(css.section, className)}>
+    <Comp
+      className={clsx(
+        css.section,
+        className,
+        style === "dark" ? css.dark : css.light
+      )}
+    >
       <div className={css.container}>{children}</div>
     </Comp>
   );
